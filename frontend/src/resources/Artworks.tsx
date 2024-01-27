2ac46f970428e46e26a555f591c0f3d527070cc5
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 09:36:16 +0200

    created on Sat Jan 27 2024 07:35:58 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Artworks.tsx
@@ -0,0 +1,93 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ArtworksTitle = () => {
+  const record = useRecordContext();
+  return <span>Artworks {record ? `"${ record.id }"` : ""}</span>;
+};
+
+export const ArtworksList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Title" />
+<TextField source="Price" />
+<TextField source="Artist" />
+<TextField source="Style" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ArtworksEdit = () => (
+                    <Edit title={<ArtworksTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Title"   />
+<TextInput source="Price"   />
+<TextInput source="Artist"   />
+<TextInput source="Style"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ArtworksCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Title"   />
+<TextInput source="Price"   />
+<TextInput source="Artist"   />
+<TextInput source="Style"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
