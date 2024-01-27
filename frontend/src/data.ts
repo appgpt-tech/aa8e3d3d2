43f9a3a833efaa93b588752bfc675bc3f386bcbc
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:30:02 +0200

    created on Sat Jan 27 2024 01:29:50 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/data.ts
@@ -0,0 +1,3 @@
+
+let data: any = {"defaultData":{"Tasks":[{"Title":"Task 1","Description":"This is task 1","Deadline":"2022-04-01","Status":"In Progress","AssignedUser":"User 1"},{"Title":"Task 2","Description":"This is task 2","Deadline":"2022-04-02","Status":"Completed","AssignedUser":"User 2"},{"Title":"Task 3","Description":"This is task 3","Deadline":"2022-04-03","Status":"Not started","AssignedUser":"User 3"}]}};
+export default data;
