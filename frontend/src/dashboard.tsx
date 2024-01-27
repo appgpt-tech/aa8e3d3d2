3465f8decd7324bb7e19cc7fc0103742d8e05d6a
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 03:30:01 +0200

    created on Sat Jan 27 2024 01:29:50 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "projectmanagementapp" />
+      <CardContent> A Project Management Application to manage tasks and teams < /CardContent>
+      </Card>
+    );
