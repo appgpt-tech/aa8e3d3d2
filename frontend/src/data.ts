e56f8b9cab6a646e02cd2832f978dadd992205e7
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 09:30:51 +0200

    edited on Sat Jan 27 2024 07:30:40 GMT+0000 (Coordinated Universal Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Tasks":[{"Title":"Task 1","Description":"This is task 1","Deadline":"2022-04-01","Status":"In Progress","AssignedUser":"User 1"},{"Title":"Task 2","Description":"This is task 2","Deadline":"2022-04-02","Status":"Completed","AssignedUser":"User 2"},{"Title":"Task 3","Description":"This is task 3","Deadline":"2022-04-03","Status":"Not started","AssignedUser":"User 3"}]}};
+let data: any = {"defaultData":{"Tasks":[{"Title":"Task 1","Description":"Description for Task 1","Deadline":"2022-01-01T00:00:00.000Z","Status":"In Progress","AssignedUser":"John Doe"},{"Title":"Task 2","Description":"Description for Task 2","Deadline":"2022-01-02T00:00:00.000Z","Status":"Not Started","AssignedUser":"Jane Doe"},{"Title":"Task 3","Description":"Description for Task 3","Deadline":"2022-01-03T00:00:00.000Z","Status":"Completed","AssignedUser":"Mark Doe"}]}};
 export default data;
